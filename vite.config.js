d6ee787a6ba7e36a18ad79c1168bfdbbca3013d5
Rowegie Lambojon <rowegie.lambojon@Rowegies-MacBook-Pro.local>
2025-02-08 16:49:53 +0800

    vite config update

--- vite.config.js
+++ vite.config.js
@@ -5,15 +5,15 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   base: "/ottoo-web",
   plugins: [react()],
-  preview: {
-    port: 8080,
-    strictPort: true,
-  },
-  server: {
-    port: 8080,
-    strictPort: true,
-    host: true,
-    origin: "http://0.0.0.0:8080",
-  },
+  // preview: {
+  //   port: 8080,
+  //   strictPort: true,
+  // },
+  // server: {
+  //   port: 8080,
+  //   strictPort: true,
+  //   host: true,
+  //   origin: "http://0.0.0.0:8080",
+  // },
 });
 
