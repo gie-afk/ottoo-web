cded15947c1c6a0575409007ae70883bbf589f47
Rowegie Lambojon <lambojon.rowegie@gmail.com>
2025-02-08 17:30:50 +0800

    revert changes for github pages

--- vite.config.js
+++ vite.config.js
@@ -3,17 +3,17 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/ottoo-web",
+  base: "/",
   plugins: [react()],
-  // preview: {
-  //   port: 8080,
-  //   strictPort: true,
-  // },
-  // server: {
-  //   port: 8080,
-  //   strictPort: true,
-  //   host: true,
-  //   origin: "http://0.0.0.0:8080",
-  // },
+  preview: {
+    port: 8080,
+    strictPort: true,
+  },
+  server: {
+    port: 8080,
+    strictPort: true,
+    host: true,
+    origin: "http://0.0.0.0:8080",
+  },
 });
 
