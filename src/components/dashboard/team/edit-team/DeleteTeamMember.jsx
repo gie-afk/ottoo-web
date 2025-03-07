970e03096041a9965c4e97adfe5c660a223e3259
James Aaron Rondero <james@tracesense.com>
2025-03-08 07:43:12 +0800

    updated team page

--- src/components/dashboard/team/edit-team/DeleteTeamMember.jsx
+++ src/components/dashboard/team/edit-team/DeleteTeamMember.jsx
@@ -2,13 +2,13 @@ import { Button } from "@heroui/react";
 const DeleteTeamMember = () => {
   return (
     <div>
-      <div className="w-full">
+      <div className="w-full flex flex-col gap-4">
         <p>
           This action cannot be undone. Once removed, <b>Cody Lindo</b> will no
           longer able to access your account.
         </p>
         <div className="w-full flex flex-row justify-end">
-          <Button color="danger">Update Permissions</Button>
+          <Button color="danger">Delete Team Member</Button>
         </div>
       </div>
     </div>
